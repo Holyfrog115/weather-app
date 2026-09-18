6e6d378824ac55a1f2e6d36362608d52d7e8c0bc
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-09-18 17:34:22 +0700

    Add response status check

--- src/weatherapi.js
+++ src/weatherapi.js
@@ -3,6 +3,10 @@ async function getApiData(location, unitGroup = "metric") {
     const response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&key=FR5HV9LKCEUMYNX5JAZAF7GPV`,
     );
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`HTTP ${response.status}: ${errorMessage}`);
+    }
     const weatherData = await response.json();
     return weatherData;
   } catch (error) {
